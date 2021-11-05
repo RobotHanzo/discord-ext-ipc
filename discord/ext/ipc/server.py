b2b4fe5e931ca6574eb93e464a56f15a1dc27089
Daishiky <77997662+Daishiky@users.noreply.github.com>
2021-11-06 00:54:50 +0100

    better error, no format or __name__

--- discord/ext/ipc/server.py
+++ discord/ext/ipc/server.py
@@ -181,8 +181,7 @@ class Server:
                         self.bot.dispatch("ipc_error", endpoint, error)
 
                         response = {
-                            "error": "IPC route raised error of type {}".format(
-                                type(error).__name__
+                            "error": f"{error}"
                             ),
                             "code": 500,
                         }
